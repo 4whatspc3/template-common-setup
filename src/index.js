9f01c203f9285711918755cf07efa0c130c3f998
Matheus <mathieus_n.7@hotmail.com>
2024-01-26 18:55:01 -0300

    Create a basic setup following the webpack documentation

---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+import './style.css';
+
+import Icon from './icon.png';
+
+import printMe from './print.js';
+
+function component() {
+    const element = document.createElement('div');
+  
+    element.textContent = 'hi';
+    element.classList.add('hello');
+
+    const btn = document.createElement('button');
+    btn.textContent = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+  
+    return element;
+}
+
+function imgComponent() {
+    const container = document.createElement('div');
+    container.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    container.appendChild(myIcon);
+  
+    return container;
+}
+
+  
+document.body.append(component(), imgComponent());
